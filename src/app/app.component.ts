2ef81c095ce39b8f650c6e26698e05d2bfbf6fbd
Alejandro Garcia Carballo <alex_garcia6@msn.com>
2025-06-10 16:40:35 -0700

    feat: minor tictactoe game player on localhost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,85 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styles: `
+  .cells {
+    width:40px;
+    height:40px;
+  }
+  `
 })
-export class AppComponent {
-  title = 'tictactoe';
+export class AppComponent implements OnInit {
+  grid: string[][] = [];
+  player = "";
+  winner = "";
+  gameOver = false;
+  winningPositions = [
+    [[0,0],[0,1],[0,2]],
+    [[1,0],[1,1],[1,2]],
+    [[2,0],[2,1],[2,2]],
+
+    [[0,0],[1,0],[2,0]],
+    [[0,1],[1,1],[2,1]],
+    [[0,2],[1,2],[2,2]],
+
+    [[0,0],[1,1],[2,2]],
+    [[2,0],[1,1],[0,2]],
+  ];
+
+  ngOnInit(): void {
+    this.startGame();
+  }
+
+  startGame() {
+    this.grid = [["","",""],["","",""],["","",""]];
+    this.player = "X";
+    this.gameOver = false;
+    this.winner = "";
+  }
+
+
+  onCellClick(row:number, column: number) {
+    if (this.gameOver) {
+      return;
+    }
+    if (this.grid[row][column] === "") {
+      this.grid[row][column] = this.player;
+      this.checkForWinner();
+      if (this.winner !== "") {
+        this.gameOver = true;
+        return;
+      }
+      this.swapPlayers();
+    }
+    this.gameOver = this.isGridFilled();
+  }
+
+  checkForWinner() {
+    const won = this.winningPositions.some((positions) =>{
+      return positions.every(position => {
+        const [row, column] = position;
+        return this.grid[row][column] === this.player;
+      });
+    })
+    if (won) {
+      this.winner = this.player;
+    }
+  }
+
+  isGridFilled(): boolean {
+    return this.grid.every((row) => row.every((cell) => cell !== ""));
+  }
+
+  swapPlayers() {
+    if (this.player === "X") {
+      this.player = "O"
+    } else {
+      this.player = "X";
+    }
+  }
+
 }
